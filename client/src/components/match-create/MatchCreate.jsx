3366c9123b27132e175b2e54fe789203af56fbdf
Simo <simeonov.pa@gmail.com>
2025-03-26 00:12:49 +0000

    Added Match Create

--- client/src/components/match-create/MatchCreate.jsx
+++ client/src/components/match-create/MatchCreate.jsx
@@ -1,11 +1,26 @@
+import { useNavigate } from "react-router";
+import matchService from "../../services/matchService.js";
+
 export default function MatchCreate() {
+    const navigate = useNavigate();
+
+    const submitAction = async (formData) => {
+        const matchData = Object.fromEntries(formData);
+        matchData.homeTeam = [];
+        matchData.awayTeam = [];
+
+        const result = await matchService.create(matchData);
+        console.log(result);
+        navigate('/');
+    };
+
     return (
        <>
             <div className="bg-[url('/images/Futsal_Commercial-1.jpg')] bg-no-repeat bg-cover bg-center h-180 pt-5">
                 <div className="content datew-7xl bg-white mx-auto text-center h-15 p-5">
-                    <form action="">
+                    <form action={submitAction}>
                         <label htmlFor="startTimedate">Date:</label>
-                        <input type="text" name="date" id="date" placeholder="14.13.2025" className="border-1" />
+                        <input type="text" name="date" id="date" placeholder="2025/03/14" className="border-1" />
                         <label htmlFor="startTime">Start Time:</label>
                         <input type="text" name="startTime" id="startTime" placeholder="15:00" className="border-1" />
                         <input type="submit" id="btn" value="Create" className="bg-[#c6ff0a] hover:bg-green-300 mx-2 py-1 px-2 w-20"/>
