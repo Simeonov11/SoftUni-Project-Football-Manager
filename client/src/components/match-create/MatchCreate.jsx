c2a59267e577ba6a86b60ee413e315a92bfbb60b
Simo <simeonov.pa@gmail.com>
2025-03-27 14:03:46 +0000

    Fixing day to Date object at CreateMatch

--- client/src/components/match-create/MatchCreate.jsx
+++ client/src/components/match-create/MatchCreate.jsx
@@ -6,6 +6,14 @@ export default function MatchCreate() {
 
     const submitAction = async (formData) => {
         const matchData = Object.fromEntries(formData);
+
+        matchData.date = new Date(matchData.date);
+        const hours = matchData.startTime.slice(0,2);
+        
+        matchData.date.setUTCHours(hours);
+        console.log(matchData);
+    
+
         matchData.homeTeam = [];
         matchData.awayTeam = [];
 
@@ -20,7 +28,7 @@ export default function MatchCreate() {
                 <div className="content datew-7xl bg-white mx-auto text-center h-15 p-5">
                     <form action={submitAction}>
                         <label htmlFor="startTimedate">Date:</label>
-                        <input type="text" name="date" id="date" placeholder="2025/03/14" className="border-1" />
+                        <input type="date" name="date" id="date" placeholder="2025/03/14" className="border-1" />
                         <label htmlFor="startTime">Start Time:</label>
                         <input type="text" name="startTime" id="startTime" placeholder="15:00" className="border-1" />
                         <input type="submit" id="btn" value="Create" className="bg-[#c6ff0a] hover:bg-green-300 mx-2 py-1 px-2 w-20"/>
--- client/src/components/matches/match-item/MatchItem.jsx
+++ client/src/components/matches/match-item/MatchItem.jsx
@@ -1,3 +1,5 @@
+import { fromIsoDate } from "../../../utils/dateTimeUtils.js";
+
 export default function MatchItem({
     _id,
     date,
@@ -7,9 +9,9 @@ export default function MatchItem({
 }) {
     return (
         <tr>
-            <td className="w-30 px-5 py-2 text-center">{date}</td>
+            <td className="w-60 px-5 py-2 text-center">{fromIsoDate(date)}</td>
             <td className="w-30 px-5 py-2 text-center">{startTime}</td>
-            <td className="w-30 px-5 py-2 text-center">_John Doe</td>
+            <td className="w-80 px-5 py-2 text-center">_John Doe</td>
             <td className="w-30 px-5 py-2 text-center">_Open</td>
             <td className="w-30 px-5 py-2 text-center">_9/10</td>
             <td className="w-30 px-5 py-2 text-center"><a href="#" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></td>
