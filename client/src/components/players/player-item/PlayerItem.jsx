d867a4498ff835bb7dae8851781ce61f65675499
Simo <simeonov.pa@gmail.com>
2025-03-28 13:44:52 +0000

    fix profile image border-radius and size

--- client/src/components/players/player-item/PlayerItem.jsx
+++ client/src/components/players/player-item/PlayerItem.jsx
@@ -11,7 +11,7 @@ export default function PlayerItem({
     return (
         <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
             <div className="flex flex-col text-center">
-                <img src={`${imageUrl}`} alt="" className="w-18 h-24 mx-auto mt-4" /><span>{firstName}</span><span>{lastName}</span><span>{position}</span><span>{rating}</span>
+                <img src={`${imageUrl}`} alt="" className="border-radius rounded-md w-20 h-24 mx-auto mt-4" /><span>{firstName}</span><span>{lastName}</span><span>{position}</span><span>{rating}</span>
             </div>
             <div className="text-center mt-4"><Link to={`/players/${_id}/details`} className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</Link></div>
         </div>
