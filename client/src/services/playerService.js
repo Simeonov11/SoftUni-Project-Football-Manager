3e55d4d53f72cac8dd478ac3353b207b4849e410
Simo <simeonov.pa@gmail.com>
2025-03-28 11:04:18 +0000

    Added playerService

---
+++ client/src/services/playerService.js
@@ -0,0 +1,26 @@
+import request from "../utils/request.js";
+
+
+const baseUrl = 'http://localhost:3030/jsonstore/players';
+
+export default {
+    async getOne(playerId) {
+        return await request.get(`${baseUrl}/${playerId}`);
+    },
+    async getAll() {
+        const result = await request.get(baseUrl);
+
+        const players = Object.values(result);
+
+        return players;
+    },
+    create(playerData) {
+        return request.post(baseUrl, playerData);
+    },
+    async edit(playerId, playerData) {
+        return await request.put(`${baseUrl}/${playerId}`, { ...playerData, _id: playerId });
+    },
+    async delete(playerId) {
+        return await request.delete(`${baseUrl}/${playerId}`);
+    }
+};
