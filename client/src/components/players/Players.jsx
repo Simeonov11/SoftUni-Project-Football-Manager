00f1f7173080c20289558c77b52ceef686e908be
Simo <simeonov.pa@gmail.com>
2025-03-28 12:05:13 +0000

    Added Players page with populated data

--- client/src/components/players/Players.jsx
+++ client/src/components/players/Players.jsx
@@ -1,8 +1,19 @@
-export default function Players() {
-    return (
-        <>
-            
+import { useEffect, useState } from "react";
+import playerService from "../../services/playerService.js";
+import PlayerItem from "./player-item/PlayerItem.jsx";
 
+export default function Players() {
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        playerService.getAll()
+            .then(result => {
+                setPlayers(result);
+            })
+    }, []);
+
+    return (
+        <>          
             <div className="bg-[url('/images/Futsal_Commercial-1.jpg')] bg-no-repeat bg-cover bg-center py-5">
                 <div className="bg-white h-12 py-3">
                     <div className="content w-7xl text-center mx-auto">
@@ -12,80 +23,7 @@ export default function Players() {
                 </div>
 
                 <div className="content w-7xl bg-white grid grid-cols-7 mx-auto p-2 mt-5">
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    <div className="bg-[url('/images/bronze.png')] bg-no-repeat bg-contain bg-center w-35 h-50 border-1 text-sm m-7 mx-auto">
-                        <div className="flex flex-col text-center">
-                            <img src="/images/ProfilePicture.png" alt="" className="w-18 mx-auto mt-4"/><span>First name</span><span>Last Name</span><span>Position</span><span>5.5</span>
-                        </div>
-                        <div className="text-center mt-4"><a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2">Details</a></div>
-                    </div>
-                    
-
+                    {players.map(player => <PlayerItem key={player._id} {...player} />)}
                 </div>
             </div>
         </>
