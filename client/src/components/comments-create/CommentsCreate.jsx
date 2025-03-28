66cbb0a22e3916319eef2ef1371d50e6f60b694f
Simo <simeonov.pa@gmail.com>
2025-03-28 17:19:22 +0000

    Edited Comments Create and Show components

--- client/src/components/comments-create/CommentsCreate.jsx
+++ client/src/components/comments-create/CommentsCreate.jsx
@@ -6,12 +6,19 @@ export default function CommentsCreate() {
                     <div className="text-2xl text-center mb-5">Add new comment:</div>
                     <div className="flex flex-col">
                         <div className="text-center mb-3">
-                            <textarea name="comments" id="comments" className="border-1 w-175 h-20">Coment text Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio reprehenderit est obcaecati!</textarea>
-                        </div>
-                        <div className="text-center mb-3">
-                            <a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2 mx-3">Edit</a>
-                            <a href="" className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2 mx-3">Delete</a>
-                        </div>
+                            <div className="text-center mb-3">
+                                <form className="form">
+                                    <textarea name="comments" id="comments" className="border-1 w-175 h-20 mb-3">Coment text Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio reprehenderit est obcaecati!</textarea>
+                                    <div className="text-center mb-3">
+                                        <input className="btn bg-[#c6ff0a] hover:bg-green-300 py-1 px-2 mx-3" type="submit" value="Add"></input>
+                                        <button className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2 mx-3">Edit</button>
+                                        <button className="bg-[#c6ff0a] hover:bg-green-300 py-1 px-2 mx-3">Delete</button>
+                                    </div>
+                                </form>
+                            </div>
+                            
+                            
+                        </div>                        
                     </div>
                 </div>
             </div>
