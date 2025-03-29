d6ae04028590930e1fbc865c40a2bc75fe983087
Simo <simeonov.pa@gmail.com>
2025-03-29 17:51:11 +0000

    Added sorting options on Matches

--- client/src/components/matches/Matches.jsx
+++ client/src/components/matches/Matches.jsx
@@ -1,12 +1,35 @@
 import MatchItem from "./match-item/MatchItem.jsx";
-import { useMatches } from "../../api/matchApi.js";
+import { useLatestMatches, useMatches } from "../../api/matchApi.js";
+import { useState } from "react";
 
 export default function Matches() {
     const { matches } = useMatches();
+    const { latestMatches } = useLatestMatches();
+
+    let matchList = latestMatches;
+
+    const [sortBy, setSortBy] = useState("createdBy"); // Default to 'createdBy'
+
+    if (sortBy === "date") {
+        matchList = latestMatches; // Sort by date
+    } else {
+        matchList = matches; // Sort by createdBy 
+    }
 
     return (
         <>
             <div className="grid auto-rows-max grid-flow-rows bg-[url('/images/Futsal_Commercial-1.jpg')] bg-no-repeat bg-cover min-h-195 bg-center py-5">
+                <div className="w-7xl bg-white justify-center mx-auto">
+                    <div className="p-2 text-center">
+                        <label htmlFor="sortBy">Sort By:</label>
+
+                        <select id="sortBy" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                            <option value="">--option--</option>
+                            <option value="createdBy" selected>CreatedBy</option>
+                            <option value="date">Date</option>
+                        </select>
+                    </div>
+                </div>
                 <div className="content w-7xl bg-white flex justify-center mx-auto pb-3">
                     <table className="table-auto">
                         <thead>
@@ -19,11 +42,11 @@ export default function Matches() {
                             </tr>
                         </thead>
                         <tbody>
-                            {Array.isArray(matches) && matches.length > 0 ? (matches.map(match => <MatchItem key={match._id} {...match} />)) : null }
+                            {Array.isArray(matchList) && matchList.length > 0 ? (matchList.map(match => <MatchItem key={match._id} {...match} />)) : null }
                         </tbody>
                     </table>
                 </div>
-                {Array.isArray(matches) && matches.length > 0 ? "" : ( <div className="notification w-7xl bg-white flex justify-center mx-auto py-3"><h2 className="no-matches">No matches scheduled</h2></div>) }
+                {Array.isArray(matchList) && matchList.length > 0 ? "" : ( <div className="notification w-7xl bg-white flex justify-center mx-auto py-3"><h2 className="no-matches">No matches scheduled</h2></div>) }
             </div>
         </>
     );
