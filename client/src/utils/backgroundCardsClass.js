4965d90866fd17f578f1b9ca98c6fc856a5e45e4
Simo <simeonov.pa@gmail.com>
2025-04-01 21:18:44 +0100

    Fix background bronz error class

--- client/src/utils/backgroundCardsClass.js
+++ client/src/utils/backgroundCardsClass.js
@@ -4,5 +4,5 @@ export const getBackgroundClass = (rating) => {
     if (rating >= 7.5) return "bg-[url('/images/playerCards/gold.png')]";
     if (rating >= 5) return "bg-[url('/images/playerCards/whiteGold.png')]";
     if (rating >= 3) return "bg-[url('/images/playerCards/silver.png')]";
-    return "[url('/images/playerCards/bronze.png')]";
+    return "bg-[url('/images/playerCards/bronze.png')]";
 }
